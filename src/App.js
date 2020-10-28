aaad7078ec46f6ca21d6e8a56b25c078bdcbc680
myllyma <matti.myllymaki83@gmail.com>
2020-10-28 15:28:33 +0200

    Improvements were made

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const CustomTable = ({itemList, identity, handleItemSelection}) => {
   const itemListEntries = itemList.map((item, index) => 
       <TableRow key={index}>
         <TableCell padding="checkbox">
-          <Checkbox onClick={handleItemSelection(identity, index)}/> 
+          <Checkbox onChange={handleItemSelection(identity, index)}/> 
         </TableCell>
         <TableCell>{item}</TableCell>
       </TableRow>
@@ -63,32 +63,67 @@ const App = () => {
       .get('http://localhost:3001/left_strings')
       .then((response) => {
         setLeftList(response.data);
-      });
+        setLeftSelected(response.data.map(item => false))
+      }
+    );
 
     axios
       .get('http://localhost:3001/right_strings')
       .then((response) => {
         setRightList(response.data);
-      });
+        setRightSelected(response.data.map(item => false));
+      }
+    );
   }, []);
+
+  // For testing purposes only.
+  useEffect(() => {
+    console.log("left selected list", leftSelected);
+    console.log("right selected list", rightSelected);
+  }, [leftSelected, rightSelected]);
+
+  // Item on one of the lists has been selected, what happens?
+  const handleItemSelection = (identity, index) => () => {
+    console.log(`item ${index} in list ${identity} selected`);
+    if (identity === "left") { // Left side box
+      setLeftSelected(leftSelected.map((item, itemIndex) => {
+        let output = false;
+        index === itemIndex ? output = !item : output = item;
+        return output;
+      }));
+    } else { // Right side box
+      setRightSelected(rightSelected.map((item, itemIndex) => {
+        let output = false;
+        index === itemIndex ? output = !item : output = item;
+        return output;
+      }));
+    }
+  }
 
   // Move selected strings from left list to right list
   const handleSwapLeftToRight = () => {
+    let newLeftList = JSON.parse(JSON.stringify(leftList));
+    let newRightList = JSON.parse(JSON.stringify(rightList));
 
+
+
+    leftList(newLeftList);
+    rightList(newRightList);
+    setLeftSelected(newLeftList.map(item => false));
+    setRightSelected(newRightList.map(item => false));
   }
 
   // Move selected strings from right list to left list
   const handleSwapRighttoLeft = () => {
+    let newLeftList = JSON.parse(JSON.stringify(leftList));
+    let newRightList = JSON.parse(JSON.stringify(rightList));
 
-  }
 
-  const handleItemSelection = (identity, index) => () => {
-    console.log(`item ${index} in list ${identity} selected`);
-    if (identity === "left") {
-      setLeftSelected(leftSelected.concat({name:leftList[index], index: index}));
-    } else {
-      setRightSelected(rightSelected.concat({name:rightList[index], index: index}));
-    }
+
+    leftList(newLeftList);
+    rightList(newRightList);
+    setLeftSelected(newLeftList.map(item => false));
+    setRightSelected(newRightList.map(item => false));
   }
 
   return (
